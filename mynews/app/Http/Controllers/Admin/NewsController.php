b6050a418bd3c99e0b29ef7d4d743bdd1e1e8b78
Ayaka_A <sbmskg3@icloud.com>
2018-12-05 21:39:01 +0900

    課題１５完了

--- mynews/app/Http/Controllers/Admin/NewsController.php
+++ mynews/app/Http/Controllers/Admin/NewsController.php
@@ -1,21 +1,55 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
+//以下を追記することでNews Modelが扱えるようになる。１５課で追記
+use App\News;
 
 class NewsController extends Controller
 {
     //以下を追記(addというActionを追加)
     public function add()
     {
-        return view( 'admin.news.create');
+        return view('admin.news.create');
     }
     //14課で追記
     public function create(Request $request)
     {
-        // admin/news/vreateにリダイレクトする
+        //以下を追記
+        //varidationを行う
+        $this->validate($request, News::$rules);
+
+        $news =new News;
+        $form =$request->all();
+
+        //フォームから画像が送信されて来たら、保存して、$news->image_pathに画像のパスを保存する
+        if (isset($form['image'])) {
+            $path =$request->file('image')->store('public/image');
+            $news->image_path = null;
+        } else {
+            $news->image_path = null;
+        }
+
+        //formから送信されて来たら。保存して、$news->image_pathに画像のパスを保存する
+        if (isset($form['image'])) {
+          $path =$request->file('image')->store('public/image');
+          $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+
+        //フォームから送信されて来た_takenを削除する
+        unset($form['_taken']);
+        //フォームから送信されて来たimageを削除する
+        unset($form['image']);
+
+        //データベースに保存する
+        $news->fill($form);
+        $news->save();
+
+        // admin/news/createにリダイレクトする
         return redirect('admin/news/create');
     }
 }
---
+++ mynews/app/profile.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class profile extends Model
+{
+    protected $guarded = array('id');
+
+    //15課で追加
+    public static $rules = array(
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'introduction'
+    );
+}
