05b6e7af86de80dd1df12fbe7411f7f7cbe4bd61
Ayaka_A <sbmskg3@icloud.com>
2018-12-20 23:51:22 +0900

    課題１７完了

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -18,8 +18,11 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::post('news/create','Admin\NewsController@create')->middleware('auth'); # 追記
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-    Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
     Route::get('news', 'Admin\NewsController@index')->middleware('auth'); //追記（＃１６）
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); //追記（＃１７）
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); //追記（＃１７）
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');//追記（＃１７）
 });
 
 Auth::routes();
